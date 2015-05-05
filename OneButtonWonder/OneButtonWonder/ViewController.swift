f360fac7f4568e90ad5f4b43df15d95f5da8fe6a
Reed Carson <rpcarson27@gmail.com>
2015-05-05 09:02:05 -0400

    simplebuttons and onebutton

---
+++ OneButtonWonder/OneButtonWonder/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  OneButtonWonder
+//
+//  Created by Reed Carson on 5/4/15.
+//  Copyright (c) 2015 Reed Carson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World",
+            message: "This is my first app!",
+            preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+  
+    
+    
+    
+}
+
